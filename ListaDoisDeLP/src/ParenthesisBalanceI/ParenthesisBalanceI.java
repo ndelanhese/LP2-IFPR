61852a46f94d01c598ee8c76ac6ed6b78b40ba11
ndelanhese <nnathanh3@gmail.com>
2022-06-29 20:01:29 -0300

    parenteses ok

--- ListaDoisDeLP/src/ParenthesisBalanceI/ParenthesisBalanceI.java
+++ ListaDoisDeLP/src/ParenthesisBalanceI/ParenthesisBalanceI.java
@@ -3,48 +3,54 @@ package ParenthesisBalanceI;
 import java.util.*;
 
 public class ParenthesisBalanceI {
-
+    
     public static void main(String[] args) {
-
+        
         Scanner leia = new Scanner(System.in);
-
+        
         while (leia.hasNext()) {
-
-            String equacao = leia.next();
-
-            String[] equacaoCortada = equacao.replaceAll("[0-9]", "").replaceAll("[aA-zZ]", "").replaceAll("[*--+/]", "").split("");
-
-            Stack<String> pilhaDeParenteses = new Stack<>();
             
-            int a = 0, b = 0;
-
+            String equacao = leia.next();
+            
+            String[] equacaoCortada = equacao.replaceAll("[0-9]", "").replaceAll("[aA-zZ]", "").replaceAll("[*--+/]", "").split("");
+            
+            Stack<String> pilhaDeParentesesAberto = new Stack<>();
+            
+            boolean verifica = false;
+            int count = 0;
             for (int i = 0; i < equacaoCortada.length; i++) {
-
+                
                 if (equacaoCortada[0].equals(")")) {
-                    i = equacaoCortada.length - 1;
+                    verifica = false;
+                    break;
                 }
-
+                
                 if (equacaoCortada[i].equals("(")) {
-
-                    pilhaDeParenteses.push("(");
+                    
+                    pilhaDeParentesesAberto.push("(");
+                    count++;
                 } else {
-                    if (equacaoCortada[i].equals(")")) {
-                        
-                        if (!pilhaDeParenteses.pop().equals("(")) {
-                             i = equacaoCortada.length - 1;
-                        }
-                        
+                    
+                   
+                    
+                    if (pilhaDeParentesesAberto.isEmpty()) {
+                        verifica = false;
+                        break;
+                    } else {
+                        pilhaDeParentesesAberto.remove(pilhaDeParentesesAberto.size()-1);
+                        verifica=true;
                     }
+                    
                 }
-                pilhaDeParenteses.pop();
+                
             }
-
-            if (a == b) {
+            
+            if (verifica && count == (equacaoCortada.length /2)) {
                 System.out.println("correct");
             } else {
                 System.out.println("incorrect");
             }
         }
     }
-
+    
 }
