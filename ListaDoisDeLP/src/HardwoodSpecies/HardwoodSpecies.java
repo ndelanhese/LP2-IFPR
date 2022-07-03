71cd584c981fe0591f54c556c500402785ef9206
ndelanhese <nnathanh3@gmail.com>
2022-07-03 18:04:02 -0300

    falta o da arvore

--- ListaDoisDeLP/src/HardwoodSpecies/HardwoodSpecies.java
+++ ListaDoisDeLP/src/HardwoodSpecies/HardwoodSpecies.java
@@ -1,11 +1,31 @@
 package HardwoodSpecies;
 
+import java.util.*;
+
 public class HardwoodSpecies {
 
     public static void main(String[] args) {
 
-        
-        
+        Scanner leia = new Scanner(System.in);
+
+        int test = leia.nextInt();
+
+        String vazio = leia.next();
+
+        ArrayList<String> species = new ArrayList<>();
+
+        for (int i = 0; i < test; i++) {
+
+            String specie = leia.nextLine();
+
+            if (!specie.equals("")) {
+
+            }
+
+        }
+
+        System.out.println(species);
+
     }
 
 }
